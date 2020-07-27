4db00600b75c4822b37d982e900c3cc06f82b159
Alberto <alsemo.alberto@gmail.com>
2020-07-27 11:05:16 +0200

    Updates

---
+++ precache-manifest.7ea94deb7281001c98fd47435c4f5b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c607c23ff14daac7633e0001b0bcdd7",
+    "url": "/githubRepFinder/index.html"
+  },
+  {
+    "revision": "ae5cd6c778f20859a9f9",
+    "url": "/githubRepFinder/static/css/main.0e7d8d16.chunk.css"
+  },
+  {
+    "revision": "e0ad6513c0e46d2cf8d6",
+    "url": "/githubRepFinder/static/js/2.9fdb57d2.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/githubRepFinder/static/js/2.9fdb57d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5cd6c778f20859a9f9",
+    "url": "/githubRepFinder/static/js/main.f76edf2c.chunk.js"
+  },
+  {
+    "revision": "9d28689a099e0fe4d327",
+    "url": "/githubRepFinder/static/js/runtime-main.6a84f968.js"
+  }
+]);
