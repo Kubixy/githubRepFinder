4ad59b89253e145d1abefee2fe6e1406481092e7
Alberto <alsemo.alberto@gmail.com>
2020-07-28 21:16:19 +0200

    Updates

---
+++ precache-manifest.0b89dc7403945ac990e02405b11faee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0c27a6c1b807c9adf08bf4b4e20ca9",
+    "url": "/githubRepFinder/index.html"
+  },
+  {
+    "revision": "47d59004a3f7b6b00371",
+    "url": "/githubRepFinder/static/css/main.0d9bff89.chunk.css"
+  },
+  {
+    "revision": "6682463d47fbf2d0c3fd",
+    "url": "/githubRepFinder/static/js/2.4c8364ff.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/githubRepFinder/static/js/2.4c8364ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d59004a3f7b6b00371",
+    "url": "/githubRepFinder/static/js/main.32cd3fba.chunk.js"
+  },
+  {
+    "revision": "9d28689a099e0fe4d327",
+    "url": "/githubRepFinder/static/js/runtime-main.6a84f968.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/githubRepFinder/precache-manifest.6f502d3bb07f7ca6fc28c9f1d49c0582.js"
+  "/githubRepFinder/precache-manifest.0b89dc7403945ac990e02405b11faee1.js"
 );
 
 self.addEventListener('message', (event) => {
